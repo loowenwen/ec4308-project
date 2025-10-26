b394a262e490f17a81256e44b84dd2f62c99760c
milivinod <e0969902@u.nus.edu>
2025-10-26 15:21:47 +0800

    Implement Z_t for F, F-X, F-Level, F-X-Level, X, X-Level

--- src/feature_engineering.R
+++ src/feature_engineering.R
@@ -1,30 +1,6 @@
 library(stats)     
 library(zoo)    
 
-#y and x matrices
-y <- fred_data_clean$CPIAUCSL
-X <- fred_data_clean %>%
-  select(-sasdate, -CPIAUCSL) %>%
-  select(where(is.numeric)) %>%
-  na.omit()
-
-# Match y to X row count after cleaning
-y <- tail(y, nrow(X))
-
-#PCA
-pca_result <- prcomp(X, scale. = TRUE)
-plot(pca_result, type = "l", main = "Scree Plot of PCA Factors", npcs = 125)
-summary(pca_result)
-num_factors <- 25
-
-# first 25 PCs
-F_t <- as.data.frame(pca_result$x[, 1:num_factors])
-colnames(F_t) <- paste0("F", 1:num_factors)
-
-#Lags for y and factors
-p_y <- 3  # Number of lags for target 
-p_f <- 3  # Number of lags for factors 
-
 # Function to create lagged variables
 create_lags <- function(df, lags) {
   lagged <- purrr::map_dfc(1:lags, ~dplyr::lag(df, .x))
@@ -32,21 +8,91 @@ create_lags <- function(df, lags) {
   return(lagged)
 }
 
-#lagged y
-y_lags <- create_lags(tibble(y_t = y), p_y)
+##Preparing data to create the various z matrices 
+X <- fred_data_clean %>%
+  select(-sasdate, -CPIAUCSL) %>%
+  select(where(is.numeric)) %>%
+  na.omit()
 
-#lagged factors (PCs)
+
+
+#PCA to create F (take first 25)
+pca_result <- prcomp(X, scale. = TRUE)
+plot(pca_result, type = "l", main = "Scree Plot of PCA Factors", npcs = 125)
+summary(pca_result)
+num_factors <- 25
+F_t <- as.data.frame(pca_result$x[, 1:num_factors])
+colnames(F_t) <- paste0("F", 1:num_factors)
+
+p_y <- 3  #Number of lags for target 
+p_f <- 3  #Number of lags for factors 
+p_m <- 3 #Number of lags for X
+
+#lagged F (LF_t)
 F_lags <- create_lags(F_t, p_f)
+#H_t
+H_t <- fred_data_values %>% select(-sasdate)
+#y_t
+y_t <- fred_data_values$CPIAUCSL  
+#lagged X_t (LX_t)
+X_t_lags <- create_lags(X, p_m)
 
-#Feature matrix Z (according to paper - yt, lagged versions of yt, current factors, lagged factors)
-Z_t <- bind_cols(
-  tibble(sasdate = tail(fred_data_clean$sasdate, nrow(X))),
-  tibble(y_t = y),
-  y_lags,
-  F_t,
-  F_lags
+
+
+##F case (LFt)
+Z_1 <- bind_cols(
+  tibble(sasdate = tail(fred_data_clean$sasdate, nrow(F_lags))),
+  F_lags  
 ) %>%
   drop_na()
 
-head(Z_t)  
-dim(Z_t)  
+##F-X case
+Z_2 <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(F_lags))),
+  F_lags,
+  X_t_lags
+) %>% drop_na()
+
+##F-level case
+Z_5 <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(F_lags))),
+  tibble(y_t = tail(y_t, nrow(F_lags))), 
+  F_lags,
+  tibble(H_t = tail(H_t, nrow(F_lags)))
+) %>% drop_na()
+
+
+##F-X-level case 
+Z_8 <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(F_lags))),
+  F_lags,
+  X_t_lags,
+  tibble(y_t = tail(y_t, nrow(F_lags))), 
+  tibble(H_t = tail(H_t, nrow(F_lags)))
+) %>% drop_na()
+
+##X
+Z_10 <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(X_t_lags))),
+  X_t_lags
+  ) %>% drop_na()
+
+
+##X-Level
+Z_15 <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(X_t_lags))),
+  X_t_lags,
+  tibble(y_t = tail(y_t, nrow(X_t_lags))), 
+  tibble(H_t = tail(H_t, nrow(X_t_lags)))
+) %>% drop_na()
+
+
+##Extra (F, F lag, y, y lag)
+y_lag <- create_lags(y_t, p_y)
+Z_a <- bind_cols(
+  tibble(sasdate = tail(fred_data_values$sasdate, nrow(F_lags))),
+  tibble(y_t = tail(y_t, nrow(F_lags))),
+  tibble(y_lag = tail(y_lag, nrow(F_lags))),
+  tibble(F_t = tail(F_t, nrow(F_lags))),
+  F_lags
+) %>% drop_na()
