666102b2795ed82a42d97c5964b7438bf2fb23c1
milivinod <e0969902@u.nus.edu>
2025-11-12 01:46:27 +0800

    scree plot

--- src/EDAplots.R
+++ src/EDAplots.R
@@ -224,14 +224,26 @@ library(scales)
 
 #######SCREE plots (decide the pc number)############
 set.seed("1234")
-pca <- prcomp(X_t, center = TRUE, scale. = TRUE) 
+pca <- prcomp(X_full, center = TRUE, scale. = TRUE) 
 var_explained <- pca$sdev^2 / sum(pca$sdev^2) 
 cum_var_explained <- cumsum(var_explained) 
 variance_table <- data.frame( PC = paste0("PC", 1:length(var_explained)), Variance_Explained = var_explained, Cumulative_Variance = cum_var_explained ) 
 print(variance_table)
 
-plot(pca, type = "l", main = "Scree Plot", col = "darkblue")
+png("../figures/scree_plot_simple.png",   # change path if you like
+    width = 1000, height = 600, res = 140)
 
+plot(pca, type = "l", main = "Scree Plot of PCA Factors (On Stationary Xs)", npcs = 125)
+abline(v = 32, col = "blue", lty = 2, lwd = 2)
+text(x = 32 + 2, y = max(pca$sdev^2) * 0.8,
+     labels = "PC32 (80% Cumulative Variance)", col = "blue", font = 0.4, adj = c(0, 0.5), cex = 1.2)
+dev.off()
+
+# Add 80% cumulative line
+cum_var <- cumsum(pca$sdev^2) / sum(pca$sdev^2)
+pc80 <- which(cum_var >= 0.8)[1]
+abline(v = pc80, col = "purple", lty = 2)
+text(pc80, max(pca$sdev^2)*0.8, paste("PC", pc80, "\n≥80%"), col = "purple", adj = c(-0.2, 1))
 
 
 
